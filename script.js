3ea2a925954e0121128890ecf85e02663bcd0635
Emelie Rosenlöw <rosenlowemelie@gmail.com>
2019-10-29 13:47:10 +0100

    Smaller/bigger text and better menubar

--- script.js
+++ script.js
@@ -10,3 +10,30 @@ function toggleHamburgerbar() {
         header.style.height = "100vh"
     }
 }
+
+function getBiggerText() {
+    const body = document.querySelector("body")
+    // body.style.fontSize = "2rem";
+
+    if (body.style.fontSize === "2rem") {
+        body.style.fontSize = "1rem"
+    }
+
+    else {
+        body.style.fontSize = "2rem"
+    }
+
+}
+
+// let scale = 1;
+
+// function enlarge() {
+//     scale *= 1.1
+//     applyScale()
+
+// }
+
+// function applyScale() {
+//     const body = document.querySelector("body");
+//     body.style.transform.fontsize = 'scale(' + scale + ')'
+// }
